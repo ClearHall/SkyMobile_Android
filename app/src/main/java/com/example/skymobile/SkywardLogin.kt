05549550032f4da06935467a4913a545ed1bc66d
Yifan Ma <hanteedoc137@gmail.com>
2019-03-28 22:47:19 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/skymobile/SkywardLogin.kt
@@ -0,0 +1,11 @@
+package com.example.skymobile
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SkywardLogin : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

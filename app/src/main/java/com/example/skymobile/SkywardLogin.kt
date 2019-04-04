ef9d361fe29ab809b43a02c41078e20a51f0c2bb
Yifan Ma <hanteedoc137@gmail.com>
2019-04-03 23:55:35 -0500

    Attempt 1 of Popup error

--- app/src/main/java/com/example/skymobile/SkywardLogin.kt
+++ app/src/main/java/com/example/skymobile/SkywardLogin.kt
@@ -1,25 +1,42 @@
 package com.example.skymobile
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.EditText
+import android.webkit.WebView
 import kotlinx.android.synthetic.main.activity_login.*
-
-const val EXTRA_MESSAGE = "com.example.SkyMobile.MESSAGE"
 
 class SkywardLogin : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loadWebView()
     }
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
         val studentID = StudentID.text.toString()
+        val password = Password.text.toString()
         println(studentID)
+        println(password)
+    }
+
+    fun loadWebView() {
+        val myWebView: WebView = WebView
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.setSupportMultipleWindows(true)
+        myWebView.settings.javaScriptCanOpenWindowsAutomatically = true
+        myWebView.loadUrl("https://skyward-fbprod.iscorp.com/scripts/wsisa.dll/WService=wsedufortbendtx/seplog01.w")
+        myWebView.evaluateJavascript("login.value = \"(UserName)\"; password.value = \"(Password)\"; bLogin.click();") {
+            val result = it
+            println(result)
+            //shouldOverrideUrlLoading(myWebView, result)
+        }
+        fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            view.loadUrl(url)
+            return true
+        }
     }
 
 }

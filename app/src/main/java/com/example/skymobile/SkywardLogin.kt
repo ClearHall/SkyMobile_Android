b79c7a6e03f703d3b5d9049c9daceb3255b7c50c
Hunter Han <hunter602353@yahoo.com>
2019-04-25 19:50:12 -0500

    Finally, got Skyward to login successfully!!!

--- app/src/main/java/com/example/skymobile/SkywardLogin.kt
+++ app/src/main/java/com/example/skymobile/SkywardLogin.kt
@@ -1,10 +1,20 @@
 package com.example.skymobile
 
 import android.os.Bundle
+import android.os.Message
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
-import android.webkit.WebView
+import android.webkit.*
 import kotlinx.android.synthetic.main.activity_login.*
+import android.webkit.WebSettings.PluginState
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+
+
+
+
 
 class SkywardLogin : AppCompatActivity(){
 
@@ -23,20 +33,42 @@ class SkywardLogin : AppCompatActivity(){
     }
 
     fun loadWebView() {
-        val myWebView: WebView = WebView
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.settings.setSupportMultipleWindows(true)
-        myWebView.settings.javaScriptCanOpenWindowsAutomatically = true
-        myWebView.loadUrl("https://skyward-fbprod.iscorp.com/scripts/wsisa.dll/WService=wsedufortbendtx/seplog01.w")
-        myWebView.evaluateJavascript("login.value = \"(UserName)\"; password.value = \"(Password)\"; bLogin.click();") {
-            val result = it
-            println(result)
-            //shouldOverrideUrlLoading(myWebView, result)
-        }
-        fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-            view.loadUrl(url)
-            return true
+        Log.d("Debug", "LEADDDDDD")
+        var cookieMan = CookieManager.getInstance()
+        cookieMan.setAcceptCookie(true)
+        val webView: WebView = WebView
+        webView.getSettings().setSupportMultipleWindows(true);
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportMultipleWindows(true)
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.loadUrl("https://skyward-fbprod.iscorp.com/scripts/wsisa.dll/WService=wsedufortbendtx/seplog01.w")
+//        myWebView.evaluateJavascript("login.value = \"(UserName)\"; password.value = \"(Password)\"; bLogin.click();") {
+//            val result = it
+//            println(result)
+//            //shouldOverrideUrlLoading(myWebView, result)
+//        }
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onCreateWindow(
+                view: WebView?,
+                isDialog: Boolean,
+                isUserGesture: Boolean,
+                resultMsg: Message?
+            ): Boolean {
+                val newWebView = WebView(applicationContext)
+                newWebView.settings.javaScriptEnabled = true
+                newWebView.settings.domStorageEnabled = true
+                newWebView.settings.setSupportZoom(true)
+                newWebView.settings.builtInZoomControls = true
+                newWebView.settings.setSupportMultipleWindows(true)
+                view!!.addView(newWebView)
+                val transport = resultMsg!!.obj as WebView.WebViewTransport
+                transport.webView = newWebView
+                resultMsg!!.sendToTarget()
+
+                newWebView.webViewClient = WebViewClient()
+
+                return true
+            }
         }
     }
-
-}
+    }

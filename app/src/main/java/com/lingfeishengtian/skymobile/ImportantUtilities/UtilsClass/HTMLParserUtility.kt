043bf65f512d688843f34ca6bf5a720660e7de25
PotatoCurry <potatocurry@protonmail.com>
2019-04-27 20:02:53 -0500

    HTML parser refactor
    Use proper Kotlin styling and best practice

--- app/src/main/java/com/lingfeishengtian/skymobile/ImportantUtilities/UtilsClass/HTMLParserUtility.kt
+++ app/src/main/java/com/lingfeishengtian/skymobile/ImportantUtilities/UtilsClass/HTMLParserUtility.kt
@@ -3,10 +3,10 @@ package com.lingfeishengtian.skymobile.ImportantUtilities.UtilsClass
 import org.jsoup.Jsoup
 
 /**
- * A very *important* utility class.
+ * A very important utility class.
  *
  * This class is a utility class used for parsing HTML and returning grades.
- * This class is very similar to the iOS version, in fact it is built from it!
+ * This class is very similar to the iOS version; in fact, it is modeled off it!
  *
  */
 
@@ -17,55 +17,42 @@ import org.jsoup.Jsoup
      * @param gradesOut The terms our parameter.
      * @return Returns the courses from the HTML.
      */
-    fun ParseGradesHTMLToRetrieveGrades(html: String, termsHTML: String, gradesOut:MutableList<String>): MutableList<Course> {
-        /**
-         * Pre-Variable Init
-         */
-        var NewCourses = mutableListOf<Course>()
-        var Classes = mutableListOf<String>()
-        val MainCSSSelectorCode = "tr[group-parent]"
-        val FinalHTML = html.replace("\\u003C", "<")
+    fun retrieveGrades(html: String, termsHTML: String, gradesOut:MutableList<String>): MutableList<Course> {
+       /** Pre-Variable Init */
+        val newCourses: MutableList<Course>
+        val classes = mutableListOf<String>()
+        val cssSelectorCode = "tr[group-parent]"
+        val finalHTML = html.replace("\\u003C", "<")
 
-        /**
-         * HTML Parsing
-         */
-        val DocumentParsed = Jsoup.parse(FinalHTML)
-        val ParentRowsFromParsedDocument = DocumentParsed.select(MainCSSSelectorCode)
+        /** HTML Parsing */
+        val document = Jsoup.parse(finalHTML)
+        val parentRows = document.select(cssSelectorCode)
 
-        /**
-         * Get Classes from parsed HTML
-         */
-        for(element in ParentRowsFromParsedDocument){
+        /** Get Classes from parsed HTML */
+        for (element in parentRows){
             val elementText = element.text()
             if(elementText.contains("Period")){
-                Classes.add(elementText)
+                classes.add(elementText)
             }
         }
 
-        /**
-         * PreInit Grades and set terms correctly.
-         */
+        /** PreInit Grades and set terms correctly. */
         for (elem in termsHTML.split("@SWIFT_TERM_SEPARATOR@")){
             if (elem.isNotEmpty() && !elem.contains("'") && !elem.contains("\"")){
                 gradesOut.add(elem)
             }
         }
 
-        /**
-         * Final init Grades and initialize Courses
-         */
-        NewCourses = SplitClassDescriptionForJSoup(Classes)
-        for (elementIndex in 0 until NewCourses.size){
-            val GradeElem = ParentRowsFromParsedDocument.get(elementIndex).select("td")
+        /** Final init Grades and initialize Courses */
+        newCourses = splitClassDescriptions(classes)
+        for (elementIndex in 0 until newCourses.size){
+            val gradeElem = parentRows.get(elementIndex).select("td")
 
-            var ind = 0
-            for (tempElem in GradeElem){
-                NewCourses.get(elementIndex).termGrades.put(gradesOut.get(ind), tempElem.text())
-                ind++
-            }
+            for ((ind, tempElem) in gradeElem.withIndex())
+                newCourses[elementIndex].termGrades[gradesOut[ind]] = tempElem.text()
         }
 
-        return NewCourses
+        return newCourses
     }
 
     /**
@@ -73,32 +60,29 @@ import org.jsoup.Jsoup
      *
      * @param classArr Array of Class Descriptions
      */
-    private fun SplitClassDescriptionForJSoup(classArr: MutableList<String>): MutableList<Course>{
-        var FinalClasses = mutableListOf<Course>()
+    private fun splitClassDescriptions(classArr: MutableList<String>): MutableList<Course>{
+        val finalClasses = mutableListOf<Course>()
 
         for (ClassDesc in classArr){
-            val SplitClassDesc = ClassDesc.split("\\n Period")
-            val Period = SplitClassDesc[1].split(")\\n ")[0].split("(")[0]
-            val ClassDescription = SplitClassDesc[0].split("\\n \\n \\n")[1]
-            var Teacher = ""
+            val splitClassDesc = ClassDesc.split("\\n Period")
+            val period = splitClassDesc[1].split(")\\n ")[0].split("(")[0]
+            val description = splitClassDesc[0].split("\\n \\n \\n")[1]
+            val teacher = if (splitClassDesc[1].split(")\\n").size <= 1)
+                splitClassDesc[1].split("(")[0].split("\\n\\n\\n")[0]
+            else
+                splitClassDesc[1].split(")\\n ")[1].split("\\n\\n\\n")[0]
 
-            if (SplitClassDesc[1].split(")\\n").size <= 1){
-                Teacher = SplitClassDesc[1].split("(")[0].split("\\n\\n\\n")[0]
-            }else {
-                Teacher = SplitClassDesc[1].split(")\\n ")[1].split("\\n\\n\\n")[0]
-            }
-
-            val CurrentCourse = Course(
-                ClassDescription,
+            val course = Course(
+                description,
                 mutableListOf(),
-                Period,
-                Teacher,
+                period,
+                teacher,
                 1.0,
                 mutableMapOf()
             )
 
-            FinalClasses.add(CurrentCourse)
+            finalClasses.add(course)
         }
 
-        return FinalClasses
+        return finalClasses
     }

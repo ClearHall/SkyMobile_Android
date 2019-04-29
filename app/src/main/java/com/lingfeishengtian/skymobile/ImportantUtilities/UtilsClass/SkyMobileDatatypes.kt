92a5d221a93d2b058091dd4307c606eded80d1a1
Hunter Han <hunter602353@yahoo.com>
2019-04-28 22:11:20 -0500

    Final assignments scraper algorithm complete

--- app/src/main/java/com/lingfeishengtian/skymobile/ImportantUtilities/UtilsClass/SkyMobileDatatypes.kt
+++ app/src/main/java/com/lingfeishengtian/skymobile/ImportantUtilities/UtilsClass/SkyMobileDatatypes.kt
@@ -5,6 +5,7 @@ import android.webkit.WebView
 var Courses = mutableListOf<Course>()
 var TermsAvailable = mutableListOf<String>()
 var SkywardWebpage: WebView? = null
+var CurrentAssignmentBlocks: AssignmentBlock? = null
 
 data class Account(
     var nickname: String,
@@ -22,9 +23,78 @@ data class Course(
 )
 
 data class Assignment(
+    var name: String,
+    var grade: Double,
+    var assignmentTag: AssignmentTag = AssignmentTag.IsAssignment
+){
+    override fun toString(): String {
+        return " Assignment: $name Grade: $grade"
+    }
+}
+
+data class AssignmentBlock(
+    val AssignmentSections: MutableList<MainAssignmentSection>
+){
+    fun PutAssignmentInLastMainAssignmentSection(assignment: Assignment){
+        AssignmentSections.last().AssignmentList.add(assignment)
+    }
+
+    fun GetLatestMainSection() : MainAssignmentSection{
+        return AssignmentSections.last()
+    }
+
+    fun GetWeightInfo() : MutableMap<String, String>{
+        val finMap = mutableMapOf<String,String>()
+        for (assignmentSection in AssignmentSections){
+            if(assignmentSection.weightIfApplicable != null){
+                finMap[assignmentSection.name] = assignmentSection.weightIfApplicable!!
+            }else{
+                for(minorSection in assignmentSection.MinorSections){
+                    finMap["${assignmentSection.name} : ${minorSection.weight}"]
+                }
+            }
+        }
+        return finMap
+    }
+
+    override fun toString(): String {
+        var tempString = ""
+        for (assignmentSec in AssignmentSections) tempString += " $assignmentSec"
+        return tempString
+    }
+}
+
+/**
+ * ALl assignments will be put under the last MAIN section created.
+ */
+data class MainAssignmentSection(
     val name: String,
-    var grade: Double
-)
+    val weightIfApplicable: String?,
+    val MinorSections: MutableList<MinorAssignmentSection>,
+    val AssignmentList: MutableList<Assignment>,
+    val grade: String?
+){
+    fun addMinorSection(minorAssignmentSection: MinorAssignmentSection){
+        MinorSections.add(minorAssignmentSection)
+    }
+
+    override fun toString(): String {
+        var tempString = "MAIN: $name $weightIfApplicable $grade "
+        for (minorSec in MinorSections) tempString += " $minorSec"
+        for (aS in AssignmentList) tempString += " $aS"
+        return tempString
+    }
+}
+
+data class MinorAssignmentSection(
+    val name: String,
+    val weight: String,
+    val grade: String
+){
+    override fun toString(): String {
+        return " Minor: $name $weight $grade"
+    }
+}
 
 data class District(
     val name: String,
@@ -34,4 +104,8 @@ data class District(
 
 enum class GPACalcSupport{
     HundredPoint, FourPoint, NoSupport
+}
+
+enum class AssignmentTag{
+    IsAssignment, IsInfoMarker
 }
--- app/src/main/java/com/lingfeishengtian/skymobile/ViewControllers/GradesRelated/AssignmentsViewController.kt
+++ app/src/main/java/com/lingfeishengtian/skymobile/ViewControllers/GradesRelated/AssignmentsViewController.kt
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import com.lingfeishengtian.skymobile.R
 
 class AssignmentsViewController : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.assignments_view)
     }
+
 
 
 }
